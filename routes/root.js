681284f44fa34d7b8b5662e34eaeebe672b6d728
muhmingreat <ayatullahmumin3@gmail.com>
2024-06-19 17:43:42 +0100

    lesson 1 backend

---
+++ routes/root.js
@@ -0,0 +1,22 @@
+// const express = require('express')
+// const router = express.Router()
+
+// const path = require('path')
+
+// // router.get('^/$]index(.html)?', (req ,res) => {
+// //     res.sendFile(path.join(__dirname, '..','views','index.html'))
+// // })
+// router.get("^/$|/index(.html)?", (req, res) => {
+//   res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+
+// });
+// module.exports = router
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
+
+module.exports = router;
